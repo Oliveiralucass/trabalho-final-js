dc73f19dff637bd8a3bd7bc60ba049b063c9f29d
Matheus <matheusrcrv07@gmail.com>
2022-10-30 17:52:24 -0300

    add: Adicionado muita coisa

--- js/script.js
+++ js/script.js
@@ -41,6 +41,11 @@ function mudarModalDetalhesVagas() {
   sectionRecTres.classList.toggle('hidden');
 };
 
+function mudarModalCandidatadoNaVaga(){
+  sectionCandTres.classList.toggle('hidden');
+  sectionRecTres.classList.toggle('blur');
+}
+
 // CLASSES DE CADASTROS
 class Usuario {
   id;
@@ -173,7 +178,7 @@ const exibirTodasAsVagas = async () => {
     const divPai = document.querySelector('.button-vagas-home-recrutador');
     const divButtonVagas = document.createElement('div');
     divButtonVagas.setAttribute('class', 'button-vagas');
-    divButtonVagas.setAttribute('onclick', `detalhesVaga(${vaga.id}); buscarCandidatos(${vaga.id})`);
+    divButtonVagas.setAttribute('onclick', `detalhesVaga(${vaga.id}); exibeDetalhesRecrutador(${vaga.id})`);
     const divFilhaUm = document.createElement('div');
     const divFilhaDois = document.createElement('div');
   
@@ -190,23 +195,36 @@ const detalhesVaga = async (idDaVaga) => {
   let vagas;
   await axios.get(URL_VAGAS).then((response) => { vagas = response.data });
 
+  let users;
+  await axios.get(URL_USUARIOS).then((response) => {users = response.data});
+
   const idVaga = document.getElementById('id-vaga');
   const salarioVaga = document.getElementById('salario-vaga');
   const tituloVaga = document.getElementById('titulo-vaga-recrutador');
   const descricaoVaga = document.getElementById('descricao-vaga-recrutador');
 
-  const candidatar = document.getElementById("candidatar")
-  candidatar.setAttribute("onclick", `candidatarVaga(${idDaVaga})`)
+  users.map((user) => {
+    if(user.id === usuarioAtivoId){
+      if(user.tipo === "candidato") {
+        const candidatar = document.getElementById("candidatar")
+        candidatar.setAttribute("onclick", `candidatarVaga(${idDaVaga})`)
+      }
+    }
+  })
 
-  vagas.filter((detalhes) => {
+  let vagaFiltrada = vagas.filter((detalhes) => {
     if(detalhes.id === idDaVaga){
+      if(detalhes.candidatos.includes(usuarioAtivoId)) return exibeTelaCadastradoNaVaga(idDaVaga);
+      buscarCandidatos(idDaVaga)
       idVaga.innerHTML = `<b>ID da Vaga:</b> ${idDaVaga}`;
       salarioVaga.innerHTML = `<b>Remuneração:</b> R$ ${detalhes.remuneracao}`;
-      tituloVaga.innerHTML = `<b>Título:</b> ${detalhes.titulo}`
+      tituloVaga.innerHTML = `<b>Título:</b> ${detalhes.titulo}`;
       descricaoVaga.innerHTML = `<b>Descrição da vaga:</b> ${detalhes.descricao}`;
+      return detalhes;
     };
   });
 
+  if(vagaFiltrada[0].candidatos.includes(usuarioAtivoId)) return
   mudarModalDetalhesVagas();
 };
 
@@ -218,7 +236,7 @@ async function candidatarVaga(idDaVaga){
   let conteudo = vagas.filter((vaga) => {
     if(vaga.id === idDaVaga) {
       if(!vaga.candidatos.includes(usuarioAtivoId)){
-        vaga.candidatos.push(usuarioAtivoId)
+        vaga.candidatos.push({ reprovado: false, userID: usuarioAtivoId })
         return vaga.candidatos
       }
     }
@@ -228,7 +246,78 @@ async function candidatarVaga(idDaVaga){
   alert('Usuario já está candidatado')
 }
 
-async function buscarCandidatos(id) {
+// Usuario está candidatado
+async function exibeTelaCadastradoNaVaga(idDaVaga){
+  let vagas;
+  await axios.get(URL_VAGAS).then((response) => { vagas = response.data });
+
+  const idVaga = document.getElementById('id-vaga-candidatado');
+  const salarioVaga = document.getElementById('salario-vaga-candidatado');
+  const tituloVaga = document.getElementById('titulo-vaga-candidatado');
+  const descricaoVaga = document.getElementById('descricao-candidato-candidatado');
+
+  vagas.filter((detalhes) => {
+    if(detalhes.id === idDaVaga){
+      idVaga.innerHTML = `<b>ID da Vaga:</b> ${idDaVaga}`;
+      salarioVaga.innerHTML = `<b>Remuneração:</b> R$ ${detalhes.remuneracao}`;
+      tituloVaga.innerHTML = `<b>Título:</b> ${detalhes.titulo}`;
+      descricaoVaga.innerHTML = `<b>Descrição da vaga:</b> ${detalhes.descricao}`;
+    };
+  });
+
+  buscarCandidatos(idDaVaga)
+  mudarModalCandidatadoNaVaga()
+}
+
+async function buscarCandidatos(idDaVaga) {
+  let vagas;
+  await axios.get(URL_VAGAS).then((response) => { vagas = response.data });
+
+  let users;
+  await axios.get(URL_USUARIOS).then((response) => {users = response.data});
+
+  users.map((tipoUser) => {
+    if(tipoUser.id === usuarioAtivoId && tipoUser.tipo === "candidato"){
+      vagas.map((vaga) => {
+        if(vaga.id === idDaVaga){
+          users.map((user) => {
+            if(vaga.candidatos.includes(user.id)){
+              const container = document.getElementById('container-candidatos');
+              const divPai = document.createElement('div');
+              divPai.classList.add('barra-nome-data-candidato-nao-cadastrado');
+              const nomeUsuario = document.createElement('div');
+              nomeUsuario.classList.add('nome-tela-recrutador'); 
+              const nascimento = document.createElement('div');
+              nascimento.classList.add('data-de-nascimento');
+              divPai.appendChild(nomeUsuario);
+              divPai.appendChild(nascimento);
+              container.appendChild(divPai);
+    
+              nomeUsuario.innerText = user.nomeCompleto;
+              nascimento.innerText = user.dataDeNascimento;
+    
+              const containerCandidatos = document.getElementById('container-candidatados');
+              const divPaiDois = document.createElement('div');
+              divPaiDois.classList.add('barra-nome-data-candidato-nao-cadastrado');
+              const nomeUsuarioDois = document.createElement('div');
+              nomeUsuarioDois.classList.add('nome-tela-recrutador'); 
+              const nascimentoDois = document.createElement('div');
+              nascimentoDois.classList.add('data-de-nascimento');
+              divPaiDois.appendChild(nomeUsuarioDois);
+              divPaiDois.appendChild(nascimentoDois);
+              containerCandidatos.appendChild(divPaiDois);
+    
+              nomeUsuarioDois.innerText = user.nomeCompleto;
+              nascimentoDois.innerText = user.dataDeNascimento;
+            };
+          });
+        };
+      });
+    };
+  });
+};
+
+async function exibeDetalhesRecrutador(idDaVaga){
   let vagas;
   await axios.get(URL_VAGAS).then((response) => { vagas = response.data });
 
@@ -236,28 +325,38 @@ async function buscarCandidatos(id) {
   await axios.get(URL_USUARIOS).then((response) => {users = response.data});
 
   vagas.map((vaga) => {
-    if(vaga.id === id){
+    if(vaga.id === idDaVaga){
       users.map((user) => {
         if(vaga.candidatos.includes(user.id)){
-          const container = document.getElementById('container-candidatos')
-          const divPai = document.createElement('div');
-          divPai.classList.add('barra-nome-data-candidato-nao-cadastrado');
-          const nomeUsuario = document.createElement('div');
-          nomeUsuario.classList.add('nome-tela-recrutador'); 
-          const nascimento = document.createElement('div');
-          nascimento.classList.add('data-de-nascimento');
+          const containerRecrutador = document.getElementById('container-recrutador');
+          const divPaiTres = document.createElement('div');
+          divPaiTres.classList.add('barra-nome-data');
 
-          divPai.appendChild(nomeUsuario);
-          divPai.appendChild(nascimento);
-          container.appendChild(divPai)
+          const nomeUsuarioTres = document.createElement('div');
+          nomeUsuarioTres.classList.add('nome-tela-recrutador');
+          const nascimentoTres = document.createElement('div');
+          nascimentoTres.classList.add('data-de-nascimento');
+          const buttonReprovar = document.createElement('button');
+          buttonReprovar.classList.add('button-aprovado-cadastrar-vaga');
+          buttonReprovar.setAttribute('onclick', 'reprovarCandidato()');
+          buttonReprovar.innerText = "Reprovar";
 
-          nomeUsuario.innerText = user.nomeCompleto;
-          nascimento.innerText = user.dataDeNascimento;
-        }
-      })
+          divPaiTres.appendChild(nomeUsuarioTres);
+          divPaiTres.appendChild(nascimentoTres);
+          divPaiTres.appendChild(buttonReprovar);
+          containerRecrutador.appendChild(divPaiTres);
+        
+          nomeUsuarioTres.innerText = user.nomeCompleto;
+          nascimentoTres.innerText = user.dataDeNascimento;
+        };
+      });
     };
   });
-};
+}
+
+function reprovarCandidato() {
+  console.log('teste')
+}
 
 /*
 const deletarVaga = async (event) => {
